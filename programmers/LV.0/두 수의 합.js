67f1da71feb437710569c6498600e9909178e191
doeun <drakequation@naver.com>
2024-01-21 18:42:05 +0900

    [add] 24/01/21

---
+++ programmers/LV.0/두 수의 합.js
@@ -0,0 +1,8 @@
+//https://school.programmers.co.kr/learn/courses/30/lessons/181846
+
+/**나의 풀이 */
+function solution(a, b) {
+  const num1 = BigInt(a);
+  const num2 = BigInt(b);
+  return `${num1 + num2}`;
+}
---
+++ programmers/LV.0/특수문자 출력하기.js
@@ -0,0 +1,12 @@
+//https://school.programmers.co.kr/learn/courses/30/lessons/181948
+
+/**나의 풀이 */
+const readline = require("readline");
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+rl.on("close", function () {
+  console.log(`!@#$%^&*(\\'"<>?:;`);
+});
