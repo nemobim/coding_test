65d8f76e0db2c5813b6fc520fb57e03d72c91ec0
nemo-nemo-bim <112849712+nemobim@users.noreply.github.com>
2024-01-22 16:38:21 +0900

    [level 0] Title: 합성수 찾기, Time: 0.05 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/120846. 합성수 찾기/합성수 찾기.js
@@ -0,0 +1,11 @@
+function solution(n) {
+  let answer = 0;
+  for (i = 4; i <= n; i++) {
+    if (i % 2 === 0) ++answer;
+    else if (i % 3 === 0) ++answer;
+    else if (i !== 5 && i % 5 === 0) ++answer;
+    else if (i !== 7 && i % 7 === 0) ++answer;
+    else if (Number.isInteger(Math.sqrt(i))) ++answer;
+  }
+  return answer;
+}
