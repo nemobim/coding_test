699d6918e5e5967080f3c0b3224c52eac5f01e93
nemo-nemo-bim <112849712+nemobim@users.noreply.github.com>
2024-01-22 16:32:53 +0900

    [level 0] Title: 커피 심부름, Time: 0.05 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/181837. 커피 심부름/커피 심부름.js
@@ -0,0 +1,13 @@
+function solution(order) {
+  let answer = 0;
+  order.forEach((x) => {
+    if (x.includes("americano")) {
+      answer += 4500;
+    } else if (x.includes("cafelatte")) {
+      answer += 5000;
+    } else if (x.includes("anything")) {
+      answer += 4500;
+    }
+  });
+  return answer;
+}
