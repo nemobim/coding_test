5d6f8264865b995eefa16ebed8f89568a89c9b28
nemo-nemo-bim <112849712+nemobim@users.noreply.github.com>
2024-01-28 22:29:23 +0900

    [level 0] Title: 문자열 계산하기, Time: 0.13 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/120902. 문자열 계산하기/문자열 계산하기.js
@@ -0,0 +1,23 @@
+/**eval 함수는 권장하지 않음 */
+// function solution(my_string) {
+//     return eval(my_string);
+// }
+
+function solution(my_string) {
+    const tokens = my_string.match(/(-?\d+)|([+-])/g);
+    
+    let result = BigInt(tokens[0]);
+
+    for (let i = 1; i < tokens.length; i += 2) {
+        const operator = tokens[i];
+        const operand = BigInt(tokens[i + 1]);
+
+        if (operator === "+") {
+            result += operand;
+        } else {
+            result -= operand;
+        }
+    }
+
+    return result;
+}
