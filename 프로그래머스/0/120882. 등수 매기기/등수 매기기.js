b404440ea7c0b7ce9224c8a6d6784148480461a3
nemo <112849712+nemobim@users.noreply.github.com>
2024-03-08 11:02:30 +0900

    [level 0] Title: 등수 매기기, Time: 0.08 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/120882. 등수 매기기/등수 매기기.js
+++ 프로그래머스/0/120882. 등수 매기기/등수 매기기.js
@@ -1,12 +1,8 @@
 function solution(score) {
   const avg = score.map((test) => test.reduce((a, b) => a + b, 0) / test.length);
-  //등수 구하기
-  const rank = avg.map((a, i) => {
-    let count = 1;
-    for (let j = 0; j < avg.length; j++) {
-      if (avg[i] < avg[j]) count++;
-    }
-    return count;
-  });
+
+  const sortedAvg = [...avg].sort((a, b) => b - a);
+  const rank = avg.map((a) => sortedAvg.indexOf(a) + 1);
+
   return rank;
 }
