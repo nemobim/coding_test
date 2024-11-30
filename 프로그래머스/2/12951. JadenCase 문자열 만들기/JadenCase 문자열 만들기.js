08e05f96c6272c838ccc980212672038e42c6cce
nemo <112849712+nemobim@users.noreply.github.com>
2024-12-01 04:17:48 +0900

    [level 2] Title: JadenCase 문자열 만들기, Time: 0.11 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/2/12951. JadenCase 문자열 만들기/JadenCase 문자열 만들기.js
+++ 프로그래머스/2/12951. JadenCase 문자열 만들기/JadenCase 문자열 만들기.js
@@ -1,6 +1,6 @@
 function solution(s) {
     return s
         .split(" ")
-        .map(x => x.length > 0 ? x[0].toUpperCase() + x.slice(1).toLowerCase() : "")
+        .map(x => isNaN(x) ? x[0].toUpperCase() + x.slice(1).toLowerCase() : "")
         .join(" ");
 }
