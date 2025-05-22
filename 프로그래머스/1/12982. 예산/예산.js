52e5663e47f8fd4b54b040731527d78bbb1c8f89
nemo <112849712+nemobim@users.noreply.github.com>
2025-05-22 23:14:55 +0900

    [level 1] Title: 예산, Time: 0.22 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/1/12982. 예산/예산.js
+++ 프로그래머스/1/12982. 예산/예산.js
@@ -1,16 +1,29 @@
+// function solution(d, budget) {
+//   let result = 0;
+//   let leftBudget = budget;
+
+//   // 비용이 적은 순으로 정렬
+//   const costSort = [...d].sort((a, b) => a - b);
+
+//   for (const cost of costSort) {
+//     if (leftBudget < cost) break; // 조기 리턴
+//     leftBudget -= cost;
+//     result++;
+//     // console.log(leftBudget, result); 
+//   }
+
+//   return result;
+// }
+
 function solution(d, budget) {
-    let result = 0;
-    let leftBudget = budget;
-    //예산이 적은 애들 먼저 처리?
-    const costSort = d.sort((a,b) => a-b);
-    
-    for(cost of costSort) {
-        //예산 초과면 조기 리턴
-        if(leftBudget < cost) return result
-        
-        leftBudget = leftBudget - cost      
-        result++ 
-        console.log(leftBudget, result)
-    }
-    return result;
+  d.sort((a, b) => a - b); // 오름차순 정렬
+
+  let sum = 0;
+
+  for (let i = 0; i < d.length; i++) {
+    sum += d[i];
+    if (sum > budget) return i; // 초과 시 그 전까지가 정답
+  }
+
+  return d.length; // 모두 처리 가능한 경우
 }
